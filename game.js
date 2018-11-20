bb23b9e69e119d4fa4ec2c4bffbf0c8815feb36c
shushi <shushi0123@126.com>
2018-11-20 23:12:58 +0800

    add a test.html file do some test

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-var canvas = wx.createCanvas();
+
+
+var canvas=document.getElementById("game_canvas");
 var context = canvas.getContext('2d')
 context.fillStyle = 'red'
 context.fillRect(0, 0, 100, 100)
